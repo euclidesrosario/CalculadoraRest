f6c3c6b20f676197b8eae72e34a2b724f4de231b
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-08-17 09:09:20 +0200

    update project

--- src/main/java/com/api/CalculadoraRest/Repository/CalculatorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.CalculadoraRest.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.CalculadoraRest.model.Calculator;
-
-public interface CalculatorRepository extends JpaRepository<Calculator, Long> {
-
-}
